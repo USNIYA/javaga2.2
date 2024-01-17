8df2a76b8708d83583999f8bd8e3855d0ff5962d
USNIYA <139346078+USNIYA@users.noreply.github.com>
2024-01-17 20:20:45 +0300

    fix: account replenishment

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int x = 200;
-        int b = 800;
-        int amount = x + b;
-        System.out.println("Итоговая сумма на счету при первом пополнении: " + amount);
+        int initialАccount = 200;
+        int replenishment = 1250;
+        int amount = initialАccount + replenishment;
+        System.out.println("Итоговый счет: " + amount);
         int bonus;
-        if (b > 1000) {
-            bonus = b / 100;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
         } else {
             bonus = 0;
         }
-        System.out.println("Количество бонусных рублей при первом пополнении: " + bonus);
-        int i = 1250;
-        int total = x + i;
-        System.out.println("Итоговая сумма на счету при втором пополнении: " + total);
-        int bonusMoney;
-        if (i > 1000) {
-            bonusMoney = i / 100;
-        } else {
-            bonusMoney = 0;
-        }
-        System.out.println("Количество бонусных рублей при втором попорлнении: " + bonusMoney);
-
-}
-
+        System.out.println("Количество бонусных рублей: " + bonus);
+    }
 }
